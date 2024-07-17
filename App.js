3d3f051b45da7f73563528ed329be5c103e3f287
Abhineet Garga <156705317+AbhineetGarga@users.noreply.github.com>
2024-07-17 23:16:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Temp from './Weather/temp';
+
+function App() {
+  return (
+    <>
+    <Temp/>
+    </>
+  );
+}
+
+export default App;
